97239ac264a43155afd40477fbe608d81a367c47
Harsha <harshitharsha778@gmail.com>
2024-01-28 23:49:53 +0530

    node changes

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const dotenv=require("dotenv");
 const cors=require("cors");
 const bodyparser=require("body-parser");
 const ConnectDB=require("./config/db");
+const path=require("path");
 
-const PORT=process.env.PORT ||5000;
+const PORT=5000;
 const app=express();
 
 // for connectinos and dotenv file
@@ -16,16 +17,20 @@ ConnectDB();
 app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'./client/build')));
 
 // app.get("/",(req,res)=>{
 //     res.send("first api");
 
 // })
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
+})
 // default route for user login and register 
 app.use("/user",require("./routes/router"));
 app.use("/borrower",require("./routes/brouter"));
 
 
 app.listen(PORT,()=>{
-    console.log(`erver is running on ${PORT}`);
+    console.log(`server is running on ${PORT}`);
 })
